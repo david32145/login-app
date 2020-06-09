022b9bab02923ef2108be74096335e3522249609
david32145 <nascimento32145@gmail.com>
2020-06-09 13:58:10 -0300

    Config projetc

---
+++ api/src/server.ts
@@ -0,0 +1,12 @@
+import app from './app'
+
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, (...err: any[]) => {
+  if (err && err.length > 0) {
+    console.log('Error on api...')
+    console.error(err)
+  } else {
+    console.log(`Server is running in http://localhost:${PORT}`)
+  }
+})
