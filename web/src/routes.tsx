aa2820dbf66afd8b5a7e065229d77b35c25682d1
david32145 <nascimento32145@gmail.com>
2020-06-12 10:46:11 -0300

    Add eslint

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,15 +1,16 @@
-import React from "react"
-import {Router as BrowserRouter, Switch, Route} from "react-router-dom"
+import React from "react";
+import { Router as BrowserRouter, Switch, Route } from "react-router-dom";
 
+import history from "utils/history";
 
-const Routes: React.FC = () => {
-  return (
-    <BrowserRouter >
-      <Switch>
-        <Route path="/login" exact>
-          <h1>Login</h1>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  ) 
-}
+const Routes: React.FC = () => (
+  <BrowserRouter history={history}>
+    <Switch>
+      <Route path="/login" exact>
+        <h1>Login</h1>
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
